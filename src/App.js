56d438e6f05e93f7f610fd2cefe8b59bb42d0174
Mohiuddin <101384674+mohiuddin-swe@users.noreply.github.com>
2022-11-04 20:13:46 +0600

    More features

--- src/App.js
+++ src/App.js
@@ -3,68 +3,75 @@ import './App.css';
 import Book from './Book';
 
 class App extends Component {
-  // State
-  // 16.8 
   state = {
     books: [
-      { bookName: "1984", writer: "George Orwell" },
-      { bookName: "The Da Vinci Code", writer: "Dan Brown" },
-      { bookName: "The Alchemist", writer: "Paulo Coelho" }
+      { id: 1, bookName: "1984", writer: "George Orwell" },
+      { id: 2, bookName: "The Da Vinci Code", writer: "Dan Brown" },
+      { id: 3, bookName: "The Alchemist", writer: "Paulo Coelho" }
     ],
-    otherProp: "I am some other Prop"
-  }
-  // constructor() {
-  //   super();
-  //   this.state = {};
-  // }
-   
-  changeBookState =()=> {
-    
-    this.setState({
-      books: [
-        { bookName: "Ninteen Eighty Four", writer: "George Orwell" },
-        { bookName: "The Da Vinci Code", writer: "Dan Brown" },
-        { bookName: "Metamorphosis", writer: "Franz kafka" }
-      ],
-    })
+    showBooks: true
   }
 
-  changeWithInput = event => {
+  changeWithInputState = (event, index) => {
+    const book = {
+      ...this.state.books[index]
+    }
+    book.bookName = event.target.value;
+    const books = [...this.state.books];
+    books[index] = book;
+
+    this.setState({ books: books });
+  }
+
+  deleteBookState = index => {
+    //const books = this.state.books.slice();
+    //const books = this.state.books.map(item => item);
+    const books = [...this.state.books];
+    books.splice(index, 1);
     this.setState({
-    books: [
-      { bookName: event.target.value, writer: "George Orwell" },
-      { bookName: "The Da Vinci Code", writer: "Dan Brown" },
-      { bookName: "The Alchemist", writer: "Paulo Coelho" }
-    ]
-  });
+      books: books
+    });
+  };
+
+  toggleBooks = () => {
+    this.setState({ showBooks: !this.state.showBooks });
   }
 
   render() {
+    const style = {
+      border: "1px solid red",
+      borderRadius: "5px",
+      backgroundColor: "black",
+      color: "white",
+    };
 
-    const style ={
-      border: "1 px solid red",
-      borderRadius:"5px",
-      backgroundColor:"black",
-      color:"white"
+    //const booksState = this.state.books;
+
+    let books = null;
+    if (this.state.showBooks) {
+      books = this.state.books.map((book, index) => {
+        return (
+          <Book
+            bookName={book.bookName}
+            writer={book.writer}
+            delete={() => this.deleteBookState(index)}
+            key={book.id}
+            inputName={(event) => this.changeWithInputState(event, index)}
+          />
+        );
+      });
     }
+
+
+    //console.log(booksState);
+    console.log(books);
+
     return (
       <div className="App">
         <h1 style={style}>Book List</h1>
-
-        <button onClick={this.changeBookState}>Change State</button>
-        <input type="text" onChange={this.changeWithInput}></input>
-
-        <Book bookName={this.state.books[0].bookName}
-         writer={this.state.books[0].writer} />
-        <Book bookName={this.state.books[1].bookName}
-         writer={this.state.books[0].writer} />
-        <Book bookName={this.state.books[2].bookName} 
-        writer={this.state.books[0].writer} />
+        <button onClick={this.toggleBooks}>Toggle Books</button>
+        {books}
       </div>
-
-
-
-
     );
   }
 }
